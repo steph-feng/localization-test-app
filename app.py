6ba48b658e196f009569479cdcfcbc137dd408b3
Vassily P <vasspetrousevitch@gmail.com>
2025-03-31 12:11:05 -0700

    minor changes to make it actually work

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from i18nilize import Localize
+from internationalize.localize import Localize
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return Localize.translate('Hello', 'french')
+    return Localize.translate('hello', 'french')
 
 if __name__ == '__main__':
     app.run()
